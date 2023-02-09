0ddcbb36b700d0f8b3f1fafad59757b66f4ba170
Phạm Hoàng Huy <87490676+HuyPhamm@users.noreply.github.com>
2023-02-09 21:14:37 +0700

    Delete B2.m

--- B2.m
+++
@@ -1,6 +0,0 @@
-V=[1 1;1 1];
-M=[V V V; V V V; V V V];%Khai trien ma tran
-M(2,:)=[];% Xoa hang 2
-M(:,3)=[];% Xoa cot 3
-z=M(4,:); % tao vecto tu hang 4
-M(4,2)=j+5; % bien doi gia tri Hang 4 Cot 2
